c2662de8d4af00d08f6b9c28a754853d93896c85
Aenimus <aenimuskol@gmail.com>
2019-12-19 00:28:19 +0000

    Fixed all the bugs; now you can give the number of banishes you wish to commit to a location without fiddling with each banish. You can change the disposable instant camera to 0 to force 8 Bobs, or otherwise the simulator cares about meeting at least one Bob.

--- Palindome_Simulator.py
+++ Palindome_Simulator.py
@@ -13,7 +13,7 @@ class Simulator():
 
     def do_quest(self, player_state):
         palindome = Palindome()
-        while palindome.progress < 4:
+        while palindome.progress < 5: #1 progress for each NC/superlikely, 1 for encountering 5 Dudes and 1 progress for either 8 dudes total or 1 Bob.
             palindome.resolve_turn(player_state, palindome)
         return player_state, palindome
 
@@ -26,10 +26,10 @@ class Simulator():
             player_state, palindome = self.do_quest(PlayerState())
             turns.append(player_state.get_total_turns_spent())
             banishes.append(palindome.get_banishes_used())
-            superlikelies.append(palindome.get_superlikelies())
+            superlikelies.append(palindome.get_superlikelies_encountered())
 
         Utils.log("In {} instances at {}% +NC, an average of {} superlikelies, an average of {} banishes and tracking {}, it took an average of {} turns to complete the quest, with a median of {} and a deviation of {}."
-        .format(iterations, PlayerState().player_nc, statistics.mean(superlikelies), statistics.mean(banishes), player_state.tracked_phylum if player_state.tracked_phylum != "" else "nothing", statistics.mean(turns), statistics.median(turns), statistics.pstdev(turns)))
+        .format(iterations, PlayerState().player_nc, statistics.mean(superlikelies), statistics.mean(banishes), player_state.tracked_phylum + "s" if player_state.tracked_phylum != "" else "nothing", statistics.mean(turns), statistics.median(turns), statistics.pstdev(turns)))
 
 
 class PlayerState():
@@ -40,17 +40,16 @@ class PlayerState():
         self.total_turns_spent = 0
         self.wishes = 3
         self.banishes = [ #"Banish Name", duration, max_available_uses, has_cooldown, is_free
-                            Banish("Spring Bumper", 30, 999, True, True),
+                            Banish("Spring Bumper", 30, 999, True, True), #Add number of banishes in a variable
                             Banish("Throw Latte", 30, 4, True, True),
-                            Banish("Reflex hammer", 30, 0, False, True),
-                            Banish("KGB dart", 20, 0, False, True),
-                            Banish("Batter Up!", 9999, 0, False, False),
-                            Banish("New Banish", 20, 0, True, False)#Available uses set to 0; will add a check for class later.
+                            Banish("Reflex hammer", 30, 3, False, True),
+                            Banish("KGB dart", 20, 3, False, True),
+                            Banish("Batter Up!", 9999, 999, False, False)
                         ]
         self.copiers = [ #"Copy Name", duration, max_available_uses, has_cooldown, number_of_copies, ignores_rejection
                             Copier("Olfaction", 40, 999, True, 3, False),
-                            Copier("Share Latte", 30, 0, True, 2),
-                            Copier("Mating Call", 999, 0, False, 1)
+                            Copier("Share Latte", 30, 3, True, 2),
+                            Copier("Mating Call", 999, 1, False, 1)
                         ]
         self.tracked_phylum = "Dude"
         self.olfacted_mob = None
@@ -59,11 +58,16 @@ class PlayerState():
         self.inventory = {
                             "ketchup hound": 0,
                             "another item": 0,
+                            "disposable instant camera": 1,
+                            "photograph of a dog": 0,
                             "I Love Me, Vol. I": 0,
                             "photograph of a red nugget": 0,
                             "photograph of an ostrich egg": 0,
                             "photograph of God": 0
                             }
+
+    def get_player_nc(self):
+        return self.player_nc
 
     def nc_mod(self):
         return mod_cap(self.player_nc)
@@ -109,6 +113,9 @@ class PlayerState():
     def incr_inventory_item(self, item):
         self.inventory[item] += 1
 
+    def decr_inventory_item(self, item):
+        self.inventory[item] -= 1
+
     def check_copier(self, location, encounter):
         for copier in self.copiers:
             if (copier.get_copied_mob(self) != encounter.get_name()) and copier.check(self):
@@ -126,10 +133,11 @@ class PlayerState():
         return avail_banish
 
     def check_banish(self, location, encounter):
-        banish = self.choose_banish(encounter)
-        if banish:
-            banish.use(location, self, encounter)
-            return True
+        if location.get_banishes_left():
+            banish = self.choose_banish(encounter)
+            if banish:
+                banish.use(location, self, encounter)
+                return True
         return False
 
     def mod_cap(virgin_mod):
@@ -266,15 +274,16 @@ class Encounter():
 
 class Location():
 
-    def __init__(self, native_nc, superlikelies, non_combats, combats):
+    def __init__(self, native_nc, superlikelies, non_combats, combats, banishes_to_commit=0):
         self.native_nc = native_nc
         self.superlikelies = superlikelies
         self.non_combats = non_combats
         self.combats = combats
         self.nc_history = collections.deque([], 5)
         self.combat_history = collections.deque([], 5)
-        self.free_turn = False
+        self.banishes_to_commit = banishes_to_commit
         self.banishes_used = 0
+        self.free_turn = False
         self.turns_spent = 0
 
     def get_non_combats(self):
@@ -314,6 +323,8 @@ class Location():
 
     def weighted_random(self, weights):
         total = sum(weight for item, weight in weights.items())
+        if not total:
+            return None
         r = random.randint(1, total)
         for (item, weight) in weights.items():
             r -= weight
@@ -322,32 +333,27 @@ class Location():
 
     def get_nc_weights(self, player_state):
         nc_weights = {}
-        for name in [x.name for x in self.non_combats if x.check(self, player_state)]:
-            if not name in nc_weights.keys():
-                copies = 1 #+ (player_state.get_extra_copies(encounter) if name not in nc_weights.keys() else 0) #I have not sorted this yet.
-                nc_weights[name] = copies if (name in self.combat_history and not player_state.get_olfacted_mob() == name) else (4 * copies)
+        for encounter in [nc for nc in self.non_combats if nc.check(self, player_state)]:
+            name = encounter.get_name()
+            copies = 1 #+ (player_state.get_extra_copies(encounter) if name not in nc_weights.keys() else 0) #I have not sorted this yet.
+            nc_weights[name] = copies if name in self.nc_history else (4 * copies)
         return nc_weights
 
-    def nc_queue(self, nc):
-        if (nc.name in self.nc_history) and (random.randrange(4)):
-            return None
-        return nc
-
     def select_nc(self, player_state):
-        if self.native_nc == 0:
+        if not len(self.non_combats):
             return None
-        actual_nc = self.native_nc + player_state.player_nc
-        avail_ncs = [x for x in self.non_combats if x.check(self, player_state)] #add in NC queue
-        if (len(avail_ncs)) and (random.randrange(100) < actual_nc):
-            encounter = None
-            while encounter is None:
-                encounter = self.nc_queue(random.choice(avail_ncs))
-            return encounter
+        actual_nc = self.native_nc + player_state.get_player_nc()
+        if actual_nc == 0:
+            return None
+        if random.randrange(100) < actual_nc:
+            return None
+        encounter_name = self.weighted_random(self.get_nc_weights(player_state))
+        if encounter_name:
+            return [nc for nc in self.non_combats if nc.name == encounter_name][0]
         return None
 
     def get_combat_weights(self, player_state):
         combat_weights = {}
-        #for name in [monster.get_name() for monster in self.combats if monster.check(player_state)]:
         for encounter in [monster for monster in self.combats if monster.check(player_state)]:
             name = encounter.get_name()
             copies = 1 + (player_state.get_extra_copies(encounter) if name not in combat_weights.keys() else 0)
@@ -356,7 +362,8 @@ class Location():
 
     def select_combat(self, player_state):
         encounter_name = self.weighted_random(self.get_combat_weights(player_state))
-        return [x for x in self.combats if x.name == encounter_name][0]
+        return [combat for combat in self.combats if combat.name == encounter_name][0]
+        return None
 
     def toggle_free_turn(self):
         self.free_turn = not self.free_turn
@@ -369,79 +376,89 @@ class Location():
             loops += 1
         if encounter is not None:
             encounter.run(self, player_state)
+            #Verbose information underneath
             #print("{}: {} at {} dudes and {} progress.".format(player_state.get_total_turns_spent(), encounter.get_name(), location.get_dudes_fought(), location.get_progress()))
 
 
 class Palindome(Location):
 
     class QuestSuperlikely(Encounter):
+
         def __init__(self, name = ""):
             self.name = name
 
         def check(self, location, player_state):
-            return location.get_pity_timer() > 5
+            return location.get_pity_timer() > 5 and (location.get_quest_items() < 3)
 
         def run(self, location, player_state):
-            location.reset_pity_timer() #Make sure it doesn't run when there are no NCs (later)
+            location.reset_pity_timer()
             ncs = location.get_non_combats()
+            available_ncs = []
             for nc in ncs:
-                if not nc.check(location, player_state):
-                    ncs.remove(nc)
-            if len(ncs):
+                if nc.check(location, player_state):
+                    available_ncs.append(nc)
+            if len(available_ncs):
                 location.incr_superlikelies()
                 chosen_nc = random.choice(ncs)
                 chosen_nc.run(location, player_state)
 
 
-    class RedNugget(Encounter): #This class is a version that uses pity timers and progress counters
+    class RedNugget(Encounter):
+
         def __init__(self, name = ""):
             self.name = name
 
         def check(self, location, player_state):
-            return player_state.get_inventory_item("photograph of a red nugget") < 1
+            return not player_state.get_inventory_item("photograph of a red nugget")
 
         def run(self, location, player_state):
             player_state.incr_inventory_item("photograph of a red nugget")
             location.incr_turns_spent()
             location.incr_progress()
             location.reset_pity_timer()
+            location.incr_quest_items()
             player_state.incr_total_turns_spent()
             self.add_nc_queue(location)
 
 
-    class OstrichEgg(Encounter): #This class is a version that uses pity timers and progress counters
+    class OstrichEgg(Encounter):
+
         def __init__(self, name = ""):
             self.name = name
 
         def check(self, location, player_state):
-            return player_state.get_inventory_item("photograph of an ostrich egg") < 1
+            return not player_state.get_inventory_item("photograph of an ostrich egg")
 
         def run(self, location, player_state):
             player_state.incr_inventory_item("photograph of an ostrich egg")
             location.incr_turns_spent()
             location.incr_progress()
             location.reset_pity_timer()
+            location.incr_quest_items()
             player_state.incr_total_turns_spent()
             self.add_nc_queue(location)
 
 
-    class God(Encounter): #This class is a version that uses pity timers and progress counters
+    class God(Encounter):
+
         def __init__(self, name = ""):
             self.name = name
 
         def check(self, location, player_state):
-            return player_state.get_inventory_item("photograph of God") < 1
+            return not player_state.get_inventory_item("photograph of God")
 
         def run(self, location, player_state):
             player_state.incr_inventory_item("photograph of God")
             location.incr_turns_spent()
             location.incr_progress()
             location.reset_pity_timer()
+            location.incr_quest_items()
             player_state.incr_total_turns_spent()
             self.add_nc_queue(location)
 
 
     class Combat(Encounter):
+
         def run(self, location, player_state):
             self.add_com_queue(location)
             location.incr_pity_timer()
@@ -457,8 +474,13 @@ class Palindome(Location):
 
 
     class DrabBard(Encounter):
+
         def check_conditional_drops(self, location, player_state):
+            photo = "photograph of a dog"
             quest_book = "I Love Me, Vol. I"
+            if (location.get_dudes_fought() > 7) and (not player_state.get_inventory_item(photo)):
+                player_state.incr_inventory_item(photo)
+                location.incr_progress()
             if (location.get_dudes_fought() == 5) and (player_state.get_inventory_item(quest_book) < 1):
                 player_state.incr_inventory_item(quest_book)
                 location.incr_progress()
@@ -480,6 +502,7 @@ class Palindome(Location):
 
 
     class Bobs(Encounter):
+
         def __init__(self, name = "", phylum = None, banish = False, copy = False):
             super().__init__(name, phylum, banish, copy)
             self.item_drops = {
@@ -493,8 +516,14 @@ class Palindome(Location):
                     player_state.incr_inventory_item(item)
 
         def check_conditional_drops(self, location, player_state):
+            photo = "photograph of a dog"
+            camera = "disposable instant camera"
             quest_book = "I Love Me, Vol. I"
-            if (location.get_dudes_fought() == 5) and (player_state.get_inventory_item(quest_book) < 1):
+            if (player_state.get_inventory_item(camera) or (location.get_dudes_fought() > 7)) and (not player_state.get_inventory_item(photo)):
+                player_state.decr_inventory_item(camera)
+                player_state.incr_inventory_item(photo)
+                location.incr_progress()
+            if (location.get_dudes_fought() == 5) and (not player_state.get_inventory_item(quest_book)):
                 player_state.incr_inventory_item(quest_book)
                 location.incr_progress()
 
@@ -528,28 +557,33 @@ class Palindome(Location):
                 Palindome.God("God")
             ],
             [   #"Combat Name", "Phylum", should_banish, should_sniff
-                Palindome.Combat("Evil Olive", "Beast", False, False),
-                Palindome.Combat("Stab Bat", "Beast", False, False),
-                Palindome.Combat("Taco Cat", "Beast", False, False),
-                Palindome.Combat("Tan Gnat", "Beast", False, False),
-                Palindome.DrabBard("Drab Bard", "Dude", False, False),
+                Palindome.Combat("Evil Olive", "Beast", True, False),
+                Palindome.Combat("Stab Bat", "Beast", True, False),
+                Palindome.Combat("Taco Cat", "Beast", True, False),
+                Palindome.Combat("Tan Gnat", "Beast", True, False),
+                Palindome.DrabBard("Drab Bard", "Dude", False, True),
                 Palindome.Bobs("Racecar Bob", "Dude", False, True),
                 Palindome.Bobs("Bob Racecar", "Dude", False, True)
-            ]
+            ],
+            2 #Number of banishes to commit to the location
         )
         self.progress = 0
-        self.dudes_fought = 0
         self.pity_timer = 0
+        self.quest_items = 0
+        self.dudes_fought = 0
         self.superlikelies_encountered = 0
+
+    def get_banishes_left(self):
+        return self.banishes_used < self.banishes_to_commit
 
     def get_progress(self):
         return self.progress
 
-    def get_pity_timer(self):
-        return self.pity_timer
-
     def incr_progress(self):
         self.progress += 1
+
+    def get_pity_timer(self):
+        return self.pity_timer
 
     def incr_pity_timer(self):
         self.pity_timer += 1
@@ -557,13 +591,19 @@ class Palindome(Location):
     def reset_pity_timer(self):
         self.pity_timer = 0
 
+    def get_quest_items(self):
+        return self.quest_items
+
+    def incr_quest_items(self):
+        self.quest_items += 1
+
     def get_dudes_fought(self):
         return self.dudes_fought
 
     def incr_dudes_fought(self):
         self.dudes_fought += 1
 
-    def get_superlikelies(self):
+    def get_superlikelies_encountered(self):
         return self.superlikelies_encountered
 
     def incr_superlikelies(self):
